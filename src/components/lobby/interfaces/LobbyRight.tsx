685d5a4cc4afc16745d07ff9e8f3a8e751cf145f
Dale <dellryuzi@gmail.com>
2023-12-29 01:14:48 +0700

    feat: add GA and theras labs

--- src/components/lobby/interfaces/LobbyRight.tsx
+++ src/components/lobby/interfaces/LobbyRight.tsx
@@ -4,12 +4,15 @@ import React from "react";
 import NewsSection from "./news";
 import Discord from "./community/discord";
 import Support from "./support";
+import Theras from "./community/theras";
 
 export default function LobbyRight() {
   return (
     <div className="absolute right-0 pt-18 z-10  h-full">
       <NewsSection />
       <Discord />
+      <br />
+      <Theras />
       <br />
       {/* <Support /> */}
       {/* FEEDBACK MODAL */}
---
+++ src/components/lobby/interfaces/community/theras.tsx
@@ -0,0 +1,25 @@
+import CornerBox from "@/components/_ui/box/CornerBox";
+import GlowingText from "@/components/_ui/text/glowing-text";
+import React from "react";
+import { FaDiscord } from "react-icons/fa";
+//
+export default function Theras() {
+  return (
+    <div
+      className="cursor-pointer"
+      onClick={() => {
+        window.open("https://theras.xyz", "_blank");
+      }}
+    >
+      <CornerBox
+        border
+        corner={false}
+        classNameOutside="mx-4 p-4 w-[300px]"
+        background="#64FFDB"
+        className="flex justify-center items-center w-full h-full uppercase"
+      >
+        <div className="text-[#142a46] font-bold">BY THERAS LABS</div>
+      </CornerBox>
+    </div>
+  );
+}
