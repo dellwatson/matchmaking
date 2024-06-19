c93e3d4f22d9109df715ece8a7abf99a016b389c
Dale <dellryuzi@gmail.com>
2024-06-19 23:24:36 +0700

    detached-multiplayroom

--- src/pages/Lobby.tsx
+++ src/pages/Lobby.tsx
@@ -1,14 +1,6 @@
-import Layout from "@/components/lobby/LayoutHeader";
-import LobbyRight from "@/components/lobby/interfaces/LobbyRight";
-import { LobbyInterfaceBot } from "@/components/lobby/LayoutHeader";
-import ShowRoom from "@components/showroom";
-import LobbyLeft from "@/components/lobby/interfaces/LobbyLeft";
 // import connect from "@/web3/connect";
 import { useEffect } from "react";
-import { erc20ABI, erc721ABI, useAccount } from "wagmi";
-import LSP8ABI from "../web3/abis/lsp8";
-import ERCGEM from "../web3/abis/erc20";
-import { parseEther } from "viem";
+
 import { Canvas } from "@react-three/fiber";
 import {
   isHost,
@@ -19,43 +11,18 @@ import {
 } from "playroomkit";
 import PlayMatch from "@/components/play/PlayMatch";
 
-export default function LobbyPage() {
-  // load from DB? and wallet?
-  // set main vehicle on lobby
+// 1. show button to go find match
+// 2. show
 
+export default function LobbyPage() {
   return (
     <div className="absolute dark:bg-black  h-full w-full !overflow-hidden">
-      {/* <LobbyLeft />
-      <LobbyRight /> */}
-      {/* <iframe
-        src="https://discord.com/widget?id=1193320377632424058&theme=dark"
-        width="350"
-        height="300"
-        allowtransparency="true"
-        frameborder="0"
-        sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts"></iframe> */}
-      {/* <Layout /> */}
-      {/* <Canvas /> */}
-      {/* <ShowRoom /> */}
-
-      {/* <LobbyInterfaceBot /> */}
       <PlayMatch />
 
-      {/* <button
-        disabled={!write}
-        onClick={() =>
-          write({
-            args: [address, 5],
-            // from: "0xA31A54e4C258B1BE8cE887a2724906BfCe88Cc6A",
-            // from: address,
-            // value: parseEther('0.01'),
-          })
-        }
-      >
-        Claim
-      </button>
-      {isLoading && <div>Check Wallet</div>}
-      {isSuccess && <div>Transaction: {JSON.stringify(data)}</div>} */}
+      {/* button lobby together joining  */}
+
+      {/* button terrain 1 */}
+      {/* button terrain 2 */}
     </div>
   );
 }
